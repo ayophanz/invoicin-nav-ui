da1b79123133ada9d4154f39bd8259057ea57457
Jford <ayophanz18@gmail.com>
2022-08-13 21:52:36 +0800

    setup ui for microfrontend

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,4 +1,24 @@
+import { ref } from 'vue';
+import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue';
+import { BookmarkAltIcon, FireIcon, HomeIcon, InboxIcon, MenuIcon, UserIcon, XIcon } from '@heroicons/vue/outline';
 export default {
     setup() {
+        const user = {
+            name: 'Emily Selman',
+            email: 'emily.selman@example.com',
+            imageUrl:
+                'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            };
+            
+            const navigation = [
+                { name: 'Home', href: '#', icon: HomeIcon },
+                { name: 'Trending', href: '#', icon: FireIcon },
+                { name: 'Bookmarks', href: '#', icon: BookmarkAltIcon },
+                { name: 'Messages', href: '#', icon: InboxIcon },
+                { name: 'Profile', href: '#', icon: UserIcon },
+            ];
+            const mobileMenuOpen = ref(false);
+        
+        return { user, mobileMenuOpen, navigation }
     }
 }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,2 +1,3 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template src="./index.html"></template>
 <script src="./index.js"></script>
