305d833520b4e58366bf30c36d13d07c7c28c3db
Jford <ayophanz18@gmail.com>
2022-08-16 11:33:59 +0800

    update

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,5 +1,10 @@
 import { ref } from 'vue';
-import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue';
+import { 
+    Dialog, 
+    DialogTitle, 
+    DialogPanel, 
+    TransitionChild, 
+    TransitionRoot } from '@headlessui/vue';
 import { 
     BookmarkAltIcon,
     FireIcon,
@@ -9,10 +14,16 @@ import {
     XIcon,
     ClipboardCheckIcon,
     AdjustmentsIcon,
-    PlusIcon } from '@heroicons/vue/outline';
+    PlusIcon,
+    SaveIcon,
+ } from '@heroicons/vue/outline';
+import { DotsVerticalIcon } from '@heroicons/vue/solid';
+
 export default {
+    name: 'Dashboard',
     components: {
         Dialog,
+        DialogTitle,
         DialogPanel,
         TransitionChild,
         TransitionRoot,
@@ -24,9 +35,12 @@ export default {
         XIcon,
         ClipboardCheckIcon,
         AdjustmentsIcon,
-        PlusIcon
+        PlusIcon,
+        SaveIcon,
+        DotsVerticalIcon
     },
     setup() {
+        /** Init vaiables */
         const user = {
             name: 'Emily Selman',
             email: 'emily.selman@example.com',
@@ -34,15 +48,42 @@ export default {
                 'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
             };
             
-            const navigation = [
-                { name: 'Customer', href: '#', icon: UsersIcon },
-                { name: 'Invoice', href: '#', icon: ClipboardCheckIcon },
-                { name: 'More', href: '#', icon: PlusIcon },
-                { name: 'Setting', href: '#', icon: AdjustmentsIcon },
-                { name: 'Profile', href: '#', icon: UserIcon },
-            ];
-            const mobileMenuOpen = ref(false);
-        
-        return { user, mobileMenuOpen, navigation }
+        const navigation = [
+            { name: 'Customer', href: '#', icon: UsersIcon },
+            { name: 'Invoice', href: '#', icon: ClipboardCheckIcon },
+            { name: 'More', href: '#', icon: PlusIcon },
+            { name: 'Setting', href: '#', icon: AdjustmentsIcon },
+            { name: 'Profile', href: '#', icon: UserIcon },
+        ];
+
+        const services = [
+            { name: 'Customer', initials: 'Cust', href: '#', description: 'Free', bgColor: 'bg-pink-600' },
+            { name: 'Product', initials: 'Prod', href: '#', description: 'Free', bgColor: 'bg-purple-600' },
+            { name: 'Order', initials: 'Ord', href: '#', description: 'Upgrade', bgColor: 'bg-yellow-500' },
+            { name: 'Invoice', initials: 'Invo', href: '#', description: 'Free', bgColor: 'bg-green-500' },
+            { name: 'Inventory', initials: 'Inve', href: '#', description: 'Upgrade', bgColor: 'bg-indigo-500' },
+        ]
+
+        const mobileMenuOpen = ref(false);
+        let   modalState     = ref(false);
+
+        /** Functions */
+        const navAction = (item) => {
+            if (item.name == 'More') {
+                modalState.value = true;
+            }
+        }
+    
+        /** Return data */
+        return { 
+            user, 
+            mobileMenuOpen, 
+            navigation,
+            modalState,
+            services,
+            SaveIcon,
+            XIcon,
+            navAction
+        }
     }
 }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,2 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template src="./index.html"></template>
 <script src="./index.js"></script>
