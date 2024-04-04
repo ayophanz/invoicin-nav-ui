9a0afdd256c45ca63d9f540d8aeb90c78aff0b3f
ayophanz <ayophanz18@gmail.com>
2024-04-04 09:03:44 +0800

    improve profile component

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -1,18 +1,38 @@
 <template>
     <div>
-        <div class="mb-5">
-            <h1 class="text-2xl font-medium leading-6 text-gray-900">Profile</h1>
-            <p class="mt-1 text-sm text-gray-500">Asterisk(*) is required fields.</p>
+        <div class="sm:hidden">
+            <label for="tabs" class="sr-only">Select a tab</label>
+            <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
+            <select id="tabs" name="tabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
+                <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
+            </select>
         </div>
-        <Form :form="compUserForm" @onchange-form="updateUserForm"></Form>
-        <div class="pt-5">
-            <div class="flex justify-center">
-                <button @click="onSave" :disabled="submitLoading" type="button" class="disabled:opacity-75 ml-3 flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    <Spinner v-if="submitLoading"></Spinner>
-                    Save
-                </button>
+        <div class="hidden sm:block">
+            <div class="border-b border-gray-200">
+                <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+                <a v-for="tab in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700', 'group inline-flex items-center border-b-2 py-4 px-1 text-sm font-medium']" :aria-current="tab.current ? 'page' : undefined">
+                    <component :is="tab.icon" :class="[tab.current ? 'text-indigo-500' : 'text-gray-400 group-hover:text-gray-500', '-ml-0.5 mr-2 h-5 w-5']" aria-hidden="true" />
+                    <span>{{ tab.name }}</span>
+                </a>
+                </nav>
             </div>
         </div>
+
+        <div v-if="profileTab()">
+            <div class="mb-5">
+                <p class="mt-5 text-sm text-gray-500">Asterisk(*) is required fields.</p>
+            </div>
+            <Form :form="compUserForm" @onchange-form="updateUserForm"></Form>
+            <div class="pt-5">
+                <div class="flex justify-center">
+                    <button @click="onSave" :disabled="submitLoading" type="button" class="disabled:opacity-75 ml-3 flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        <Spinner v-if="submitLoading"></Spinner>
+                        Save
+                    </button>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 <script setup lang="ts">
@@ -23,6 +43,7 @@
     import accountService from './../services/account';
     import { useToast } from "vue-toastification";
     import formTraits from '../traits/formTraits.js';
+    import { KeyIcon, UserIcon } from '@heroicons/vue/solid';
 
     const toast = useToast();
     const accountStore = useAccountStore();
@@ -51,8 +72,17 @@
         },
     });
 
+    const tabs = ref([
+        { name: 'Profile', href: '#', icon: UserIcon, current: true },
+        { name: 'Password', href: '#', icon: KeyIcon, current: false },
+    ]);
+
+    const profileTab = () => {
+        const key = tabs.value.findIndex((x: { name: string; }) => x.name === 'Profile');
+        return key >= 0 ? tabs.value[key].current : false;
+    };
+
     const updateUserForm = (value: any) => {
-        console.log(value.value);
         userForm.value[value.name].value = value.value;
     };
 
