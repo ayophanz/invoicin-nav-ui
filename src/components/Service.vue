9e7725fa9a5ba761855cae10166d7e3e0901afaf
ayophanz <ayophanz18@gmail.com>
2024-05-29 11:51:47 +0800

    improve naming

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -30,7 +30,11 @@
               'flex-shrink-0 flex items-center justify-center w-16 text-white text-sm font-medium rounded-l-md',
             ]"
           >
-            <component :is="service.icon" class="h-6 w-6" aria-hidden="true" />
+            <Component
+              :is="service.icon"
+              class="h-6 w-6"
+              aria-hidden="true"
+            ></Component>
           </div>
           <div
             class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate"
